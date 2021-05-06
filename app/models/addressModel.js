8107a27f6003a5f4e1757e4761ce5b61d7d3d38f
chaerulmarwan20 <chaerulmarwan20@gmail.com>
2021-05-06 11:45:58 +0700

    fix endpoint address

--- app/models/addressModel.js
+++ app/models/addressModel.js
@@ -1,24 +1,120 @@
 const connection = require("../configs/dbConfig");
 
-exports.insertAddress = (data) => {
+exports.getAllAddress = (queryPage, queryPerPage, sortBy, order, idUser) => {
   return new Promise((resolve, reject) => {
-    connection.query("INSERT INTO address SET ?", data, (err, resultInsert) => {
-      if (!err) {
+    connection.query(
+      "SELECT COUNT(*) AS totalData FROM address WHERE idUser = ?",
+      idUser,
+      (err, result) => {
+        let totalData, page, perPage, totalPage;
+        if (err) {
+          reject(new Error("Internal server error"));
+        } else {
+          totalData = result[0].totalData;
+          page = queryPage ? parseInt(queryPage) : 1;
+          perPage = queryPerPage ? parseInt(queryPerPage) : 5;
+          totalPage = Math.ceil(totalData / perPage);
+        }
+        const firstData = perPage * page - perPage;
         connection.query(
-          "SELECT name FROM users WHERE id = ?",
-          data.idUser,
+          `SELECT * FROM address WHERE idUser = ? ORDER BY ${sortBy} ${order} LIMIT ?, ?`,
+          [idUser, firstData, perPage],
           (err, result) => {
-            if (!err) {
-              resolve(result);
+            if (err) {
+              reject(new Error("Internal server error"));
             } else {
-              reject(new Error(err));
+              resolve([totalData, totalPage, result, page, perPage]);
             }
           }
         );
-      } else {
-        reject(new Error(err));
       }
-    });
+    );
+  });
+};
+
+exports.getAddressById = (id) => {
+  return new Promise((resolve, reject) => {
+    connection.query(
+      "SELECT * FROM address WHERE idUser = ? AND isPrimary = true LIMIT 1",
+      id,
+      (err, result) => {
+        if (!err) {
+          resolve(result);
+        } else {
+          reject(new Error("Internal server error"));
+        }
+      }
+    );
+  });
+};
+
+exports.insertAddress = (data) => {
+  return new Promise((resolve, reject) => {
+    connection.query(
+      "SELECT * FROM address WHERE idUser = ? AND isPrimary = true",
+      data.idUser,
+      (err, result) => {
+        if (!err) {
+          if (result.length > 0 && data.isPrimary === true) {
+            connection.query(
+              "UPDATE address SET isPrimary = false WHERE idUser = ?",
+              data.idUser,
+              (err, result) => {
+                if (!err) {
+                  connection.query(
+                    "INSERT INTO address SET ?",
+                    data,
+                    (err, result) => {
+                      if (!err) {
+                        connection.query(
+                          "SELECT * FROM address WHERE idUser = ?",
+                          data.idUser,
+                          (err, result) => {
+                            if (!err) {
+                              resolve(result);
+                            } else {
+                              reject(new Error("Internal Server Error"));
+                            }
+                          }
+                        );
+                      } else {
+                        reject(new Error("Internal Server Error"));
+                      }
+                    }
+                  );
+                } else {
+                  reject(new Error("Internal Server Error"));
+                }
+              }
+            );
+          } else {
+            connection.query(
+              "INSERT INTO address SET ?",
+              data,
+              (err, result) => {
+                if (!err) {
+                  connection.query(
+                    "SELECT * FROM address WHERE idUser = ?",
+                    data.idUser,
+                    (err, result) => {
+                      if (!err) {
+                        resolve(result);
+                      } else {
+                        reject(new Error("Internal Server Error"));
+                      }
+                    }
+                  );
+                } else {
+                  reject(new Error("Internal Server Error"));
+                }
+              }
+            );
+          }
+        } else {
+          reject(new Error("Internal Server Error"));
+        }
+      }
+    );
   });
 };
 
@@ -29,27 +125,91 @@ exports.updateAddress = (
   postalCode,
   city,
   name,
-  phoneNumber
+  phoneNumber,
+  isPrimary,
+  idUser
 ) => {
   return new Promise((resolve, reject) => {
     connection.query(
-      "SELECT * FROM address where id = ?",
-      idAddress,
+      "SELECT * FROM address WHERE idUser = ? AND isPrimary = true",
+      idUser,
       (err, result) => {
         if (!err) {
-          connection.query(
-            "UPDATE address SET type = ?, address = ?, postalCode = ?, city = ?, name = ?, phoneNumber = ? WHERE id = ? ",
-            [type, address, postalCode, city, name, phoneNumber, idAddress],
-            (err, resultUpdate) => {
-              if (!err) {
-                resolve(resultUpdate);
-              } else {
-                reject(new Error(err));
+          if (result.length > 0 && result[0].isPrimary === 1) {
+            connection.query(
+              "UPDATE address SET isPrimary = false WHERE idUser = ?",
+              idUser,
+              (err, result) => {
+                if (!err) {
+                  connection.query(
+                    "UPDATE address SET type = ?, address = ?, postalCode = ?, city = ?, name = ?, phoneNumber = ?, isPrimary = ? WHERE id = ? ",
+                    [
+                      type,
+                      address,
+                      postalCode,
+                      city,
+                      name,
+                      phoneNumber,
+                      isPrimary,
+                      idAddress,
+                    ],
+                    (err, result) => {
+                      if (!err) {
+                        connection.query(
+                          "SELECT * FROM address WHERE id = ?",
+                          idAddress,
+                          (err, result) => {
+                            if (!err) {
+                              resolve(result);
+                            } else {
+                              reject(new Error("Internal server error"));
+                            }
+                          }
+                        );
+                      } else {
+                        reject(new Error("Internal Server Error"));
+                      }
+                    }
+                  );
+                } else {
+                  reject(new Error("Internal Server Error"));
+                }
               }
-            }
-          );
+            );
+          } else {
+            connection.query(
+              "UPDATE address SET type = ?, address = ?, postalCode = ?, city = ?, name = ?, phoneNumber = ?, isPrimary = ? WHERE id = ? ",
+              [
+                type,
+                address,
+                postalCode,
+                city,
+                name,
+                phoneNumber,
+                isPrimary,
+                idAddress,
+              ],
+              (err, result) => {
+                if (!err) {
+                  connection.query(
+                    "SELECT * FROM address WHERE id = ?",
+                    idAddress,
+                    (err, result) => {
+                      if (!err) {
+                        resolve(result);
+                      } else {
+                        reject(new Error("Internal server error"));
+                      }
+                    }
+                  );
+                } else {
+                  reject(new Error("Internal Server Error"));
+                }
+              }
+            );
+          }
         } else {
-          reject(new Error(err));
+          reject(new Error("Internal Server Error"));
         }
       }
     );

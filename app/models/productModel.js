7fd18dd35df24a227c45a1227a08a58b0320da1b
chaerulmarwan20 <chaerulmarwan20@gmail.com>
2021-05-06 23:20:06 +0700

    add endpoint for product

---
+++ app/models/productModel.js
@@ -0,0 +1,158 @@
+const connection = require("../configs/dbConfig");
+
+exports.getAllProduct = (
+  queryPage,
+  queryPerPage,
+  keyword,
+  sortBy,
+  orderBy,
+  size,
+  color,
+  category,
+  brand
+) => {
+  return new Promise((resolve, reject) => {
+    connection.query(
+      "SELECT COUNT(*) AS totalData FROM ((product INNER JOIN category ON product.idCategory = category.id) INNER JOIN store ON product.idStore = store.id) WHERE product.title LIKE ? AND product.size LIKE ? AND product.color LIKE ? AND category.name LIKE ? AND store.name LIKE ?",
+      [
+        `%${keyword}%`,
+        `%${size}%`,
+        `%${color}%`,
+        `%${category}%`,
+        `%${brand}%`,
+      ],
+      (err, resultCount) => {
+        let totalData, page, perPage, totalPage;
+        if (err) {
+          reject(new Error("Internal Server Error"));
+        } else {
+          totalData = resultCount[0].totalData;
+          page = queryPage ? parseInt(queryPage) : 1;
+          perPage = queryPerPage ? parseInt(queryPerPage) : 5;
+          totalPage = Math.ceil(totalData / perPage);
+        }
+        const firstData = perPage * page - perPage;
+        connection.query(
+          `SELECT product.id, product.title, category.name AS category, store.name AS brand, product.image, product.size, product.color, product.price, product.conditions, product.description, product.rating, product.isPopular FROM ((product INNER JOIN category ON product.idCategory = category.id) INNER JOIN store ON product.idStore = store.id) WHERE product.title LIKE ? AND product.size LIKE ? AND product.color LIKE ? AND category.name LIKE ? AND store.name LIKE ? ORDER BY ${sortBy} ${orderBy} LIMIT ?, ?`,
+          [
+            `%${keyword}%`,
+            `%${size}%`,
+            `%${color}%`,
+            `%${category}%`,
+            `%${brand}%`,
+            firstData,
+            perPage,
+          ],
+          (err, result) => {
+            if (!err) {
+              resolve([totalData, totalPage, result, page, perPage]);
+            } else {
+              reject(new Error("Internal Server Error"));
+            }
+          }
+        );
+      }
+    );
+  });
+};
+
+exports.getAllPopular = (queryPage, queryPerPage, sortBy, orderBy) => {
+  return new Promise((resolve, reject) => {
+    connection.query(
+      "SELECT COUNT(*) AS totalData FROM ((product INNER JOIN category ON product.idCategory = category.id) INNER JOIN store ON product.idStore = store.id) WHERE product.isPopular = true",
+      (err, resultCount) => {
+        let totalData, page, perPage, totalPage;
+        if (err) {
+          reject(new Error("Internal Server Error"));
+        } else {
+          totalData = resultCount[0].totalData;
+          page = queryPage ? parseInt(queryPage) : 1;
+          perPage = queryPerPage ? parseInt(queryPerPage) : 5;
+          totalPage = Math.ceil(totalData / perPage);
+        }
+        const firstData = perPage * page - perPage;
+        connection.query(
+          `SELECT product.id, product.title, category.name AS category, store.name AS brand, product.image, product.price, product.conditions, product.description, product.rating, product.isPopular FROM ((product INNER JOIN category ON product.idCategory = category.id) INNER JOIN store ON product.idStore = store.id) WHERE product.isPopular = true ORDER BY ${sortBy} ${orderBy} LIMIT ?, ?`,
+          [firstData, perPage],
+          (err, result) => {
+            if (!err) {
+              resolve([totalData, totalPage, result, page, perPage]);
+            } else {
+              reject(new Error("Internal Server Error"));
+            }
+          }
+        );
+      }
+    );
+  });
+};
+
+exports.getProductById = (id) => {
+  return new Promise((resolve, reject) => {
+    connection.query(
+      "SELECT product.id, product.title, category.name AS category, store.name AS brands, product.image, product.price, product.conditions, product.description, product.rating, product.isPopular FROM ((product INNER JOIN category ON product.idCategory = category.id) INNER JOIN store ON product.idStore = store.id) WHERE product.id = ?",
+      id,
+      (err, result) => {
+        if (!err) {
+          resolve(result);
+        } else {
+          reject(new Error("Internal server error"));
+        }
+      }
+    );
+  });
+};
+
+exports.getProductByCategory = (
+  queryPage,
+  queryPerPage,
+  sortBy,
+  orderBy,
+  id
+) => {
+  return new Promise((resolve, reject) => {
+    connection.query(
+      "SELECT COUNT(*) AS totalData FROM ((product INNER JOIN category ON product.idCategory = category.id) INNER JOIN store ON product.idStore = store.id) WHERE category.id = ?",
+      id,
+      (err, resultCount) => {
+        let totalData, page, perPage, totalPage;
+        if (err) {
+          reject(new Error("Internal Server Error"));
+        } else {
+          totalData = resultCount[0].totalData;
+          page = queryPage ? parseInt(queryPage) : 1;
+          perPage = queryPerPage ? parseInt(queryPerPage) : 5;
+          totalPage = Math.ceil(totalData / perPage);
+        }
+        const firstData = perPage * page - perPage;
+        connection.query(
+          `SELECT product.id, product.title, category.name AS category, store.name AS brand, product.image, product.price, product.conditions, product.description, product.rating, product.isPopular FROM ((product INNER JOIN category ON product.idCategory = category.id) INNER JOIN store ON product.idStore = store.id) WHERE category.id = ? ORDER BY ${sortBy} ${orderBy} LIMIT ?, ?`,
+          [id, firstData, perPage],
+          (err, result) => {
+            if (!err) {
+              resolve([totalData, totalPage, result, page, perPage]);
+            } else {
+              reject(new Error("Internal Server Error"));
+            }
+          }
+        );
+      }
+    );
+  });
+};
+
+exports.getAllImageProduct = (id) => {
+  return new Promise((resolve, reject) => {
+    connection.query(
+      "SELECT gallery.id, gallery.image FROM gallery INNER JOIN product ON gallery.idProduct = product.id WHERE product.id = ?",
+      id,
+      (err, result) => {
+        if (!err) {
+          resolve(result);
+        } else {
+          reject(new Error("Internal server error"));
+        }
+      }
+    );
+  });
+};
--- app/routers/index.js
+++ app/routers/index.js
@@ -4,11 +4,13 @@ const route = express.Router();
 const usersRouter = require("./usersRouter");
 const addressRouter = require("./addressRouter");
 const orderRouter = require("./orderRouter");
+const productRouter = require("./productRouter");
 const categoryRouter = require("./categoryRouter");
 
 route.use("/users", usersRouter);
 route.use("/address", addressRouter);
 route.use("/order", orderRouter);
+route.use("/product", productRouter);
 route.use("/category", categoryRouter);
 
 module.exports = route;

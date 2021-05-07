bc54ec7917332423d9ed5b7214a7890a8b2c3c15
chaerulmarwan20 <chaerulmarwan20@gmail.com>
2021-05-07 20:50:38 +0700

    add all endpoint for store/brand

--- app/controllers/storeController.js
+++ app/controllers/storeController.js
@@ -1,145 +1,341 @@
-const storeModel = require("../models/storeModel")
-const usersModel = require("../models/usersModel")
+const storeModel = require("../models/storeModel");
+const usersModel = require("../models/usersModel");
 const helper = require("../helpers/printHelper");
-const validation = require("../helpers/validation")
+const validation = require("../helpers/validation");
 const fs = require("fs");
 const path = require("path");
 
 exports.findStore = (req, res) => {
-    const userID = req.auth.id
-    storeModel
-        .findStore(userID)
-        .then((result) => {
-            helper.printSuccess(res, 200, "Find one store successfull", result);
-        })
-        .catch((err) => {
-            helper.printError(res, 500, err.message);
-        })
-}
-exports.myProduct = async (req, res) => {
-    const userID = req.auth.id
-    const { sortBy, orderBy, page, perPage } = req.query
+  const id = req.auth.id;
 
-
-    const result = await storeModel.findStore(userID)
-    const { id } = result[0]
-    storeModel
-        .myProduct(id, sortBy, orderBy, page, perPage)
-        .then(([totalData, totalPage, result, page, perPage]) => {
-            helper.printPaginate(
-                res,
-                200,
-                "Find all address successfully",
-                totalData,
-                totalPage,
-                result,
-                page,
-                perPage
-            );
-        })
-        .catch((err) => {
-            helper.printError(res, 500, err.message);
-        })
-
-}
-exports.insertProduct = async (req, res) => {
-    const userID = req.auth.id
-    const { title, price, conditions, description, idCategory } = req.body
-
-    const resultStore = await storeModel.findStore(userID)
-    if (resultStore < 1) {
-        helper.printError(res, 400, "Store not found");
+  storeModel
+    .findStore(id)
+    .then((result) => {
+      if (result.length === 0) {
+        helper.printError(res, 400, `Cannot find store with idUser = ${id}`);
         return;
-    }
-    const idStore = resultStore[0].id
-    const data = {
-        idCategory,
-        idStore,
-        title,
-        price,
-        conditions,
-        description,
-        totalSale: 0,
-        rating: 0,
-        isPopular: 0
-    }
-    const resultProduct = await storeModel.findProduct(idStore, title)
-    if (resultProduct[0].totalProduct > 1) {
-        helper.printError(res, 400, "this product already exist, please input another product / new product name");
-        return;
-    }
+      }
+      helper.printSuccess(res, 200, "Find one store successfull", result);
+    })
+    .catch((err) => {
+      helper.printError(res, 500, err.message);
+    });
+};
 
-    storeModel
-        .insertProduct(data)
-        .then((result) => {
-            helper.printSuccess(res, 200, "insert product successfull", result);
-        })
-        .catch((err) => {
-            helper.printError(res, 500, err.message);
-        })
-}
 exports.updateStore = async (req, res) => {
-    const validate = validation.validationUpdateStore(req.body)
-    
-    if (validate.error) {
-        helper.printError(res, 400, validate.error.details[0].message);
-        return;
+  const validate = validation.validationUpdateStore(req.body);
+
+  if (validate.error) {
+    helper.printError(res, 400, validate.error.details[0].message);
+    return;
+  }
+
+  const id = req.auth.id;
+
+  const { storeName, email, phoneNumber, description } = req.body;
+
+  const dataStore = {
+    name: storeName,
+    description,
+  };
+  const dataUser = {
+    email,
+    phoneNumber,
+  };
+
+  try {
+    const checkStore = await storeModel.checkStore(id, storeName);
+    if (checkStore.length > 0) {
+      helper.printError(res, 400, "Store name is already in use");
+      return;
     }
-    
-    const id = 5 //req.auth.id;
+  } catch (err) {
+    helper.printError(res, 500, err.message);
+    return;
+  }
 
-    const { storeName, email, phoneNumber, description } = req.body;
-
-    const dataStore = {
-        name: storeName,
-        description
-    }
-    const dataUser = {
-        email,
-        phoneNumber
-    };
-
-    usersModel
-        .findUser(id, "update")
+  storeModel
+    .findStore(id, "update")
+    .then((result) => {
+      let image;
+      if (!req.file) {
+        image = result[0].image;
+      } else {
+        const oldImage = result[0].image;
+        if (oldImage !== "images\\default_store.jpg") {
+          removeImage(oldImage);
+        }
+        image = req.file.path;
+      }
+      dataStore.image = image;
+      return usersModel.updateUsers(id, dataUser);
+    })
+    .then((result) => {
+      storeModel
+        .updateStore(id, dataStore)
         .then((result) => {
-            return usersModel.updateUsers(id, dataUser);
-        })
-        .then((result) => {
-            delete result[0].password;
-            delete result[0].active;
-            delete result[0].createdAt;
-            delete result[0].updatedAt;
-
-            let image;
-            if (!req.file) {
-                image = result[0].image;
-            } else {
-                const oldImage = result[0].image;
-                if (oldImage !== "images\\default.png") {
-                    removeImage(oldImage);
-                }
-                image = req.file.path;
-            }
-            dataStore.image = image;
-            storeModel
-                .updateStore(id, dataStore)
-                .then((result) => {
-                    helper.printSuccess(res, 200, "update store successfull", result);
-                })
-                .catch((err) => {
-                    helper.printError(res, 500, err.message);
-                })
-            //   helper.printSuccess(res, 200, "Users has been updated", result);
+          delete result[0].createdAt;
+          delete result[0].updatedAt;
+          helper.printSuccess(res, 200, "Store has been updated", result);
         })
         .catch((err) => {
-            if (err.message === "Internal server error") {
-                helper.printError(res, 500, err.message);
-            }
-            helper.printError(res, 400, err.message);
+          helper.printError(res, 500, err.message);
         });
+    })
+    .catch((err) => {
+      if (err.message === "Internal server error") {
+        helper.printError(res, 500, err.message);
+      }
+      helper.printError(res, 400, err.message);
+    });
+};
 
-}
-const removeImage = (filePath) => {
-    filePath = path.join(__dirname, "../..", filePath);
-    fs.unlink(filePath, (err) => new Error(err));
+exports.findAllProduct = async (req, res) => {
+  const userID = req.auth.id;
+  const { page, perPage } = req.query;
+  const keyword = req.query.keyword ? req.query.keyword : "";
+  const sortBy = req.query.sortBy ? req.query.sortBy : "product.id";
+  const order = req.query.order ? req.query.order : "ASC";
+
+  let id;
+  try {
+    const result = await storeModel.findStore(userID);
+    id = result[0].id;
+  } catch (err) {
+    helper.printError(res, 500, err.message);
+  }
+
+  storeModel
+    .getAllProduct(id, sortBy, order, page, perPage, keyword)
+    .then(([totalData, totalPage, result, page, perPage]) => {
+      if (result < 1) {
+        helper.printError(res, 400, "Product not found");
+        return;
+      }
+      helper.printPaginate(
+        res,
+        200,
+        "Find all product successfully",
+        totalData,
+        totalPage,
+        result,
+        page,
+        perPage
+      );
+    })
+    .catch((err) => {
+      helper.printError(res, 500, err.message);
+    });
+};
+
+exports.findAllProductSold = async (req, res) => {
+  const userID = req.auth.id;
+  const { page, perPage } = req.query;
+  const sortBy = req.query.sortBy ? req.query.sortBy : "product.id";
+  const order = req.query.order ? req.query.order : "ASC";
+
+  let id;
+  try {
+    const result = await storeModel.findStore(userID);
+    id = result[0].id;
+  } catch (err) {
+    helper.printError(res, 500, err.message);
+  }
+
+  storeModel
+    .getAllProductSold(id, sortBy, order, page, perPage)
+    .then(([totalData, totalPage, result, page, perPage]) => {
+      if (result < 1) {
+        helper.printError(res, 400, "Product sold out not found");
+        return;
+      }
+      helper.printPaginate(
+        res,
+        200,
+        "Find all product sold out successfully",
+        totalData,
+        totalPage,
+        result,
+        page,
+        perPage
+      );
+    })
+    .catch((err) => {
+      helper.printError(res, 500, err.message);
+    });
+};
+
+exports.findAllProductArchive = async (req, res) => {
+  const userID = req.auth.id;
+  const { page, perPage } = req.query;
+  const sortBy = req.query.sortBy ? req.query.sortBy : "product.id";
+  const order = req.query.order ? req.query.order : "ASC";
+
+  let id;
+  try {
+    const result = await storeModel.findStore(userID);
+    id = result[0].id;
+  } catch (err) {
+    helper.printError(res, 500, err.message);
+  }
+
+  storeModel
+    .getAllProductArchive(id, sortBy, order, page, perPage)
+    .then(([totalData, totalPage, result, page, perPage]) => {
+      if (result < 1) {
+        helper.printError(res, 400, "Product archive not found");
+        return;
+      }
+      helper.printPaginate(
+        res,
+        200,
+        "Find all product archive successfully",
+        totalData,
+        totalPage,
+        result,
+        page,
+        perPage
+      );
+    })
+    .catch((err) => {
+      helper.printError(res, 500, err.message);
+    });
+};
+
+exports.createProduct = async (req, res) => {
+  const image = req.files;
+  const id = req.auth.id;
+  const {
+    title,
+    idCategory,
+    price,
+    conditions,
+    description,
+    stock,
+    size,
+    color,
+  } = req.body;
+
+  const resultStore = await storeModel.findStore(id);
+  if (resultStore < 1) {
+    helper.printError(res, 400, "Store not found");
+    return;
+  }
+  const idStore = resultStore[0].id;
+  const data = {
+    title,
+    idCategory,
+    idStore,
+    image: image[0].path,
+    price,
+    conditions,
+    description,
+    size: JSON.stringify(size),
+    color: JSON.stringify(color),
+    stock,
+    rating: 0,
+    isPopular: 0,
+    isArchived: 0,
   };
+  const resultProduct = await storeModel.findProduct(idStore, title);
+  if (resultProduct[0].totalProduct > 0) {
+    helper.printError(
+      res,
+      400,
+      "This product already exist, please input another product / new product name"
+    );
+    return;
+  }
+
+  storeModel
+    .insertProduct(data)
+    .then((result) => {
+      const idProduct = result[0].id;
+      image.map(async (item, index) => {
+        const dataGallery = {
+          idProduct,
+          image: item.path,
+        };
+        await storeModel.insertGallery(dataGallery);
+      });
+      delete result[0].createdAt;
+      delete result[0].updatedAt;
+      helper.printSuccess(res, 200, "New product has been created", result);
+    })
+    .catch((err) => {
+      helper.printError(res, 500, err.message);
+    });
+};
+
+exports.findAllOrder = async (req, res) => {
+  const userID = req.auth.id;
+  const { page, perPage } = req.query;
+  const keyword = req.query.keyword ? req.query.keyword : "";
+  const status = req.query.status ? req.query.status : "";
+  const sortBy = req.query.sortBy ? req.query.sortBy : "transaction.id";
+  const order = req.query.order ? req.query.order : "DESC";
+
+  let id;
+  try {
+    const result = await storeModel.findStore(userID);
+    id = result[0].id;
+  } catch (err) {
+    helper.printError(res, 500, err.message);
+  }
+
+  storeModel
+    .getAllOrder(page, perPage, sortBy, order, id, status, keyword)
+    .then(([totalData, totalPage, result, page, perPage]) => {
+      if (result < 1) {
+        helper.printError(res, 400, "Order not found");
+        return;
+      }
+      helper.printPaginate(
+        res,
+        200,
+        "Find all order successfully",
+        totalData,
+        totalPage,
+        result,
+        page,
+        perPage
+      );
+    })
+    .catch((err) => {
+      helper.printError(res, 500, err.message);
+    });
+};
+
+exports.findAllDetail = (req, res) => {
+  const id = req.params.id;
+  const { page, perPage } = req.query;
+  const sortBy = req.query.sortBy ? req.query.sortBy : "detail_transaction.id";
+  const order = req.query.order ? req.query.order : "ASC";
+
+  storeModel
+    .getAllDetailOrder(page, perPage, sortBy, order, id)
+    .then(([totalData, totalPage, result, page, perPage]) => {
+      if (result < 1) {
+        helper.printError(res, 400, `Cannot find order detail with id = ${id}`);
+        return;
+      }
+      helper.printPaginate(
+        res,
+        200,
+        "Find order detail successfully",
+        totalData,
+        totalPage,
+        result,
+        page,
+        perPage
+      );
+    })
+    .catch((err) => {
+      helper.printError(res, 500, err.message);
+    });
+};
+
+const removeImage = (filePath) => {
+  filePath = path.join(__dirname, "../..", filePath);
+  fs.unlink(filePath, (err) => new Error(err));
+};
--- app/routers/storeRouter.js
+++ app/routers/storeRouter.js
@@ -4,9 +4,51 @@ const storeController = require("../controllers/storeController");
 const auth = require("../middlewares/auth");
 const multer = require("../middlewares/multer");
 
-router.get("/find-store", auth.verification(), storeController.findStore);
-router.get("/myproduct", auth.verification(), storeController.myProduct);
-router.post("/insert-product", auth.verification(), storeController.insertProduct); //kurang multiple upload image
-router.put("/update-store", multer.uploadImage.single("image"), storeController.updateStore);
+router
+  .get("/", auth.verification(), auth.isSeller(), storeController.findStore)
+  .get(
+    "/product",
+    auth.verification(),
+    auth.isSeller(),
+    storeController.findAllProduct
+  )
+  .get(
+    "/product/sold",
+    auth.verification(),
+    auth.isSeller(),
+    storeController.findAllProductSold
+  )
+  .get(
+    "/product/archive",
+    auth.verification(),
+    auth.isSeller(),
+    storeController.findAllProductArchive
+  )
+  .post(
+    "/",
+    auth.verification(),
+    auth.isSeller(),
+    multer.uploadImage.array("image", 5),
+    storeController.createProduct
+  )
+  .put(
+    "/",
+    auth.verification(),
+    auth.isSeller(),
+    multer.uploadImage.single("image"),
+    storeController.updateStore
+  )
+  .get(
+    "/order",
+    auth.verification(),
+    auth.isSeller(),
+    storeController.findAllOrder
+  )
+  .get(
+    "/order/:id",
+    auth.verification(),
+    auth.isSeller(),
+    storeController.findAllDetail
+  );
 
 module.exports = router;
